066c54081c5641e4afd505fd044840bd21f2a009
tfields1234 <57992547+tfields1234@users.noreply.github.com>
2021-03-27 10:31:55 -0400

    add first GET route

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const express = require("express");
 //This next creates an instance of the express constructor, which we will name "app".
 const app = express();
 
+const mockUserData = [{ name: "Mark" }, { name: "Jill" }];
+
+//adding mock data to the json file
+app.get("/users", function (req, res) {
+    res.json({
+        success: true,
+        message: "successfully got users. Nice!",
+        users: mockUserData,
+    });
+});
+
 //The app.listen method will start up the server locally on the port you give as its first argument (in this case the base url is: http://localhost:8000)
 app.listen(8000, function () {
     console.log("server is running");
