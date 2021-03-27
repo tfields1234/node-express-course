6a8b8e4f0ec5b671b8012abe0dca93a7f875e25d
tfields1234 <57992547+tfields1234@users.noreply.github.com>
2021-03-27 10:40:51 -0400

    add dynamic GET route

--- server.js
+++ server.js
@@ -14,6 +14,15 @@ app.get("/users", function (req, res) {
     });
 });
 
+app.get("/users/:id", function (req, res) {
+    console.log(req.params.id);
+    res.json({
+        success: true,
+        message: "got one user",
+        user: req.params.id,
+    });
+});
+
 //The app.listen method will start up the server locally on the port you give as its first argument (in this case the base url is: http://localhost:8000)
 app.listen(8000, function () {
     console.log("server is running");
