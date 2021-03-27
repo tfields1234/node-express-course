7ae3f2b44d6a9b0b79728c67397864557c7ff725
tfields1234 <57992547+tfields1234@users.noreply.github.com>
2021-03-27 10:51:33 -0400

    add login POST route

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get("/users", function (req, res) {
     });
 });
 
+// colons are used as variables that be viewed in the params
 app.get("/users/:id", function (req, res) {
     console.log(req.params.id);
     res.json({
@@ -21,6 +22,32 @@ app.get("/users/:id", function (req, res) {
         message: "got one user",
         user: req.params.id,
     });
+});
+
+//A POST request can send data securely through the request body. In order to make POST requests, first we need to include the "body-parser" library from our node_modules (included with express).
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+//Let's write a function to handle a POST request made to the 'login' endpoint, as if a user was trying to log in:
+app.post("/login", function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "billyTheKid";
+    const mockPassword = "superSecret";
+
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+            success: true,
+            message: "password and username match!",
+            token: "encrypted token goes here",
+        });
+    } else {
+        res.json({
+            success: false,
+            message: "password and username do not match",
+        });
+    }
 });
 
 //The app.listen method will start up the server locally on the port you give as its first argument (in this case the base url is: http://localhost:8000)
